bce43b96d265d3efeaad5f95422288c85231d236
Robert Malsagov <ryzone3@gmail.com>
2025-05-11 03:34:09 +0300

    h

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
-var image = document.querySelector('.img');
-
-image.addEventListener(click, function() {
-  image.classList.toggle('.show');
-});
+function animateit() {
+  document.querySelector(".onefre").classList.toggle("block-spin-one");
+  document.querySelector(".twofre").classList.toggle("block-spin-two");
+  document.querySelector(".entering").classList.toggle("entering-end");
+  document.querySelector(".btn_text").classList.toggle("btn_text-end");
+}
